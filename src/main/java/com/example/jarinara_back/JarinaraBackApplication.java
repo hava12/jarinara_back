fbd7cf71848d0efc6c761605eedbfcbcb0b726b6
dongyeop <hava12@naver.com>
2022-04-24 21:22:54 +0900

    Initial commit

---
+++ src/main/java/com/example/jarinara_back/JarinaraBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.jarinara_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarinaraBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarinaraBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jarinara_back/JarinaraBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jarinara_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarinaraBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
