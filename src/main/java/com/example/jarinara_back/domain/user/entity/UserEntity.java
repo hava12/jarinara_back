cef121e55c59bc7861da0bb1ba6b3047cd11df9f
dongyeop <hava12@naver.com>
2022-04-25 22:08:54 +0900

    Spring h2 Database/jpa 초기 설정 2

---
+++ src/main/java/com/example/jarinara_back/domain/user/entity/UserEntity.java
@@ -0,0 +1,22 @@
+package com.example.jarinara_back.domain.user.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Entity // 테이블과 매핑한다고 JPA에게 알려줌 엔티티 클래스
+@Table(name="USER") // 매핑할 테이블 정보를 알려준다.
+@Data
+public class UserEntity {
+
+	@Id // 기본 키
+	@Column(name = "user_id") // 필드를 컬럼에 매핑
+	private String id;
+
+	@Column(name = "user_name")
+	private String userName;
+
+	private Integer age; // 매핑 어노테이션 생략 시 필드명을 사용해서 컬럼명으로 매핑한다.
+}
