2095265f7b1530bef1777be9422d6fc44ed8d392
dongyeop <hava12@naver.com>
2022-04-28 01:29:31 +0900

    Spring security 초기 설정 1

---
+++ src/main/java/com/example/jarinara_back/config/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.example.jarinara_back.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.formLogin()
+			// 로그인 즉 인증 처리를 하는 URL을 설정합니다.
+			// 호출되면 인증처리를 수행하는 필터가 호출됩니다.
+			.loginProcessingUrl("/login");
+
+		http.authorizeRequests()
+			.antMatchers("/**").permitAll();
+	}
+}
